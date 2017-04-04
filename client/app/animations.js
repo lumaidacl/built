4bf7e8e231abb633f1e0f457953f07cde93d452b
Carlitos <carlitos@Carloss-MacBook-Pro.local>
2017-04-04 12:04:50 -0400

    Deleted JS functions

--- client/app/animations.js
+++ client/app/animations.js
@@ -1,6 +1,1 @@
-$(function(){
-  $(".welcome").typed({
-    strings: ["Hi! :) <br>My name is Julio Silva<br>You can follow me on <br>@juliosguz"],
-    typeSpeed: 120
-  });
-});
+
--- client/app/content.js
+++ client/app/content.js
@@ -1,3 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	console.log("Hello :) My name is Julio Silva, you can follow me on @juliosguz");
-});
+
